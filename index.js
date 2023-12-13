e02fab5e817a1e52b5a7061c390dffd8100e5281
sshmatrix <cryptosauron@protonmail.com>
2023-12-13 15:39:26 +0530

    npx dev3 cleanup

--- index.js
+++ index.js
@@ -2,11 +2,17 @@
 import { init } from "./init.js"
 import { publish } from "./publish.js"
 import graphics from './utils/graphics.js'
-const arg = process.argv.slice(2)[0];
+const arg = process.argv.slice(2)[0]
 if (arg === "init") {
     init()
 } else if (arg === "sign") {
     publish()
 } else {
-    console.log("Dev3, Please Run \n   > dev3 init \nOR\n   > dev3 sign\nVisit: https://dev3.eth.limo for docs.")
+    // WELCOME!
+    graphics.print(graphics.asciiArt, 'orange')
+    graphics.logo()
+    console.log()
+    graphics.print("👉 Please run `npx dev3 init` to initialise", "skyblue")
+    graphics.print("👉 Please run `npx dev3 sign` to sign & publish your ENS records", "skyblue")
+    graphics.print(" ◥ docs: https://dev3.eth.limo", "skyblue")
 }
